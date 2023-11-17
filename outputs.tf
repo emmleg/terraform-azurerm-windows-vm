92233fbd50e59acd2d585c37ff77edccdc1b50bc
Emmanuel Legris <emmanuel.legris@outlook.com>
2023-11-17 16:34:34 -0500

    Outputs

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "id" {
+  description = "The ID of the Virtual Machine."
+  value       = azurerm_windows_virtual_machine.vm.id
+}
+
+output "name" {
+  description = "The Name of the Virtual Machine."
+  value       = azurerm_windows_virtual_machine.vm.name
+}
+
+output "computer_name" {
+  description = "The Comuputer/Host name of the Virtual Machine."
+  value       = azurerm_windows_virtual_machine.vm.computer_name
+}
+
+output "public_ip_address" {
+  description = "Public IP Address of the Virtual Machine."
+  value       = try(azurerm_public_ip.public_ip.ip_address, null)
+}
+
+output "private_ip_address" {
+  description = "Private IP Address of the Virtual Machine."
+  value       = try(azurerm_network_interface.nic.private_ip_address, null)
+}
